ad0020a41b9ac787c7dee878d3237f84f603e633
doaafathy115 <doaafathy472@gmail.com>
2020-09-25 13:32:45 +0200

    First Task

---
+++ Assignment 01.py
@@ -0,0 +1,22 @@
+# -----------------------------
+# --- This Is The First Task --
+# --- From Lesson 1 To 10 -----
+# -----------------------------
+
+# Type String
+name = "Doaa"
+
+# Type Number
+age = 29
+
+# Type String
+country = "German"
+
+print(type(name))
+print(type(age))
+print(type(country))
+
+result = ("Hello " + name + " Your Age Is " +
+          str(age) + " Your Country Is " + country)
+
+print(result)
